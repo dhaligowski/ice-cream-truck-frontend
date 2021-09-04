89f17f1052f30ea2eae58bb42b32be983480ea82
DHaligowski <dhaligowski@gmail.com>
2021-09-04 14:14:22 -0400

    Update production apiURL

--- config/api.js
+++ config/api.js
@@ -1,4 +1,4 @@
-// const apiURL = "ws://192.168.0.xx:3000";
+//const apiURL = "ws://192.168.0.xx:3000";
 //const apiURL = "ws://192.168.1.xx:3000";
 const apiURL = "https://pacific-basin-20318.herokuapp.com/";
 
